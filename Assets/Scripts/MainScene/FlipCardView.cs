6dc3023f7f512261f0ba3216035e8885452766fb
ArthurHsieh <arthur14038@gmail.com>
2016-03-02 21:12:47 +0800

    新增FlipCardUI

---
+++ Assets/Scripts/MainScene/FlipCardView.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+
+public class FlipCardView : AbstractView
+{
+	public VoidNoneParameter onClickBack;
+	public VoidNoneParameter onClickPlay;
+	public RectTransform group_FlipCard;
+	public RectTransform image_ShakeCircle;
+
+	public override IEnumerator Init()
+	{
+		escapeEvent = OnClickBack;
+		yield return null;
+	}
+
+	public void OnClickBack()
+	{
+		AudioManager.Instance.PlayOneShot("Button_Click2");
+		if(onClickBack != null)
+			onClickBack();
+	}
+
+	public void OnClickPlay()
+	{
+		AudioManager.Instance.PlayOneShot("Button_Click");
+		if(onClickPlay != null)
+			onClickPlay();
+	}
+
+	public IEnumerator ShakeEffect(RectTransform shakeItem, float enterDuration)
+	{
+		shakeItem.rotation = Quaternion.Euler(Vector3.zero);
+		yield return shakeItem.DORotate(Vector3.forward * 10f, enterDuration).SetEase(Ease.OutBounce).WaitForCompletion();
+		yield return shakeItem.DORotate(Vector3.back * 7.5f, 0.125f).SetEase(Ease.OutQuad).WaitForCompletion();
+		yield return shakeItem.DORotate(Vector3.forward * 5.0f, 0.125f).SetEase(Ease.OutQuad).WaitForCompletion();
+		yield return shakeItem.DORotate(Vector3.back * 2.5f, 0.125f).SetEase(Ease.OutQuad).WaitForCompletion();
+		yield return shakeItem.DORotate(Vector3.zero, 0.125f).SetEase(Ease.OutQuad).WaitForCompletion();
+	}
+
+	protected override IEnumerator HideUIAnimation()
+	{
+		group_FlipCard.anchoredPosition = Vector2.zero;
+		yield return group_FlipCard.DOAnchorPos(hideRight, 0.5f).SetEase(Ease.OutCubic).WaitForCompletion();
+		base.HideUI(false);
+		hideCoroutine = null;
+	}
+
+	protected override IEnumerator ShowUIAnimation()
+	{
+		StartCoroutine(ShakeEffect(image_ShakeCircle, 0.5f));
+		group_FlipCard.anchoredPosition = hideRight;
+		yield return group_FlipCard.DOAnchorPos(Vector2.zero, 0.5f).SetEase(Ease.OutCubic).WaitForCompletion();
+		showCoroutine = null;
+	}
+}
