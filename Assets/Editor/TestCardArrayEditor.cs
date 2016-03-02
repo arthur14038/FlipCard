4011ec7fbfce29ed23b6e3b1085b9e8dde57ff4c
ArthurHsieh <arthur14038@gmail.com>
2016-03-02 08:30:05 +0800

    修改陣列用場景與腳本

---
+++ Assets/Editor/TestCardArrayEditor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+[CustomEditor(typeof(TestCardArray))]
+public class TestCardArrayEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		TestCardArray myTarget = (TestCardArray)target;
+
+		myTarget.cardParent = EditorGUILayout.ObjectField("Card Parent", myTarget.cardParent, typeof(Transform), true) as Transform;
+		myTarget.useLayout = (UseLayout)EditorGUILayout.EnumPopup("Use Layout", myTarget.useLayout);
+		switch(myTarget.useLayout)
+		{
+			case UseLayout.LevelDifficulty:
+				myTarget.difficulty = (LevelDifficulty)EditorGUILayout.EnumPopup("Difficulty", myTarget.difficulty);
+				break;
+			case UseLayout.Infinite:
+				myTarget.totalCardCount = EditorGUILayout.IntField("Total Card Count", myTarget.totalCardCount);
+				myTarget.cardSize = EditorGUILayout.FloatField("Card Size", myTarget.cardSize);
+				break;
+		}
+
+		if(EditorApplication.isPlaying)
+			if(GUILayout.Button("Show Cards"))
+			{
+				myTarget.LoadAndShowCards();
+			}
+		//EditorGUILayout.LabelField("Level", myTarget.Level.ToString());
+	}
+}
--- Assets/Scripts/TestCardArray.cs
+++ Assets/Scripts/TestCardArray.cs
@@ -3,35 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 
-[CustomEditor(typeof(TestCardArray))]
-public class TestCardArrayEditor : Editor
-{
-	public override void OnInspectorGUI()
-	{
-		TestCardArray myTarget = (TestCardArray)target;
-
-		myTarget.cardParent = EditorGUILayout.ObjectField("Card Parent", myTarget.cardParent, typeof(Transform), true) as Transform;
-		myTarget.useLayout = (UseLayout)EditorGUILayout.EnumPopup("Use Layout", myTarget.useLayout);
-		switch(myTarget.useLayout)
-		{
-			case UseLayout.LevelDifficulty:
-				myTarget.difficulty = (LevelDifficulty)EditorGUILayout.EnumPopup("Difficulty", myTarget.difficulty);
-				break;
-			case UseLayout.Infinite:
-				myTarget.totalCardCount = EditorGUILayout.IntField("Total Card Count", myTarget.totalCardCount);
-				myTarget.cardSize = EditorGUILayout.FloatField("Card Size", myTarget.cardSize);
-				break;
-		}
-		
-		if(EditorApplication.isPlaying)
-			if(GUILayout.Button("Show Cards"))
-			{
-				myTarget.LoadAndShowCards();
-			}
-		//EditorGUILayout.LabelField("Level", myTarget.Level.ToString());
-	}
-}
-
 public enum UseLayout {LevelDifficulty, Infinite}
 public class TestCardArray : MonoBehaviour {
 	public Transform cardParent;
